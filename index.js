68188048267d57c681ab51770da0c0bd8ddaa0f0
Hugh A <harcenea@gmail.com>
2018-06-04 18:42:46 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,29 @@
+var musicians = [John, Ringo, Paul, George];
+var instruments = [Voice, Drum, Bass, Guitar];
+var facts = ["la",
+  "de",
+  "da",
+  "doop"];
+var num = 0;
+
+function theBeatlesPlay(musicians, instruments) {
+  var BeatlesLoops = [];
+  for (var i = 0; i < 4; i++) 
+  {BeatlesLoops.push(musicians[i] +" plays " +  instruments[i]);}  
+  return BeatlesLoops;
+}
+
+function johnLennonFacts(facts) 
+{var BeatlesLoops = [];
+var i;
+{BeatlesLoops.push("!!!"); i++}
+while (i < 4) 
+return BeatlesLoops;
+}
+
+function iLoveTheBeatles(num)
+{
+  var BeatlesLoops= [];
+do {BeatlesLoops.push("I love the Beatles!"); num++ } while (num < 15);
+  return BeatlesLoops;
+}
